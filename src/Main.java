6f8860494daab194c5631fa8c01646ffe4874ffe
Andrei Gorshkov <andruhinsans@mail.ru>
2024-08-15 18:30:18 +0300

    for exam 2

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,12 @@ public class Main {
         Book number2 = new Book("Отцы и дети", name2, 1862);
 
         System.out.println("number1.getTitleOfBook() = " + number1.getTitleOfBook());
-        System.out.println("number1.getAuthor().firstName = " + number1.getAuthor().firstName);
-        System.out.println("number1.getAuthor().lasstName = " + number1.getAuthor().lastName);
+        System.out.println("number1.getAuthor().firstName = " + number1.getAuthor().getFirstName());
+        System.out.println("number1.getAuthor().lasstName = " + number1.getAuthor().getLastName());
         System.out.println("number1.getPublicationYear() = " + number1.getPublicationYear() + "\n");
         System.out.println("number2.getTitleOfBook() = " + number2.getTitleOfBook());
-        System.out.println("number2.getAuthor().firstName = " + number2.getAuthor().firstName);
-        System.out.println("number2.getAuthor().lasstName = " + number2.getAuthor().lastName);
+        System.out.println("number2.getAuthor().firstName = " + number2.getAuthor().getFirstName());
+        System.out.println("number2.getAuthor().lasstName = " + number2.getAuthor().getLastName());
         System.out.println("number2.getPublicationYear() = " + number2.getPublicationYear() + "\n");
 
         number1.setPublicationYear(1916);
